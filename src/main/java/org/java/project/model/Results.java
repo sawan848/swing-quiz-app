2ff0f3807cb6b0d95ae029303e90b8d57526ccf0
sawan848 <ksawan802@gmail.com>
2022-02-09 20:24:01 +0530

    create result model/entity

---
+++ src/main/java/org/java/project/model/Results.java
@@ -0,0 +1,50 @@
+package org.java.project.model;
+
+
+import jakarta.persistence.*;
+
+/**
+ * 09/02/2022
+ */
+
+@Entity
+public class Results {
+    @Id
+    private String email;
+    private int totalScore;
+
+
+    public Results() {
+        super();
+    }
+
+
+    public Results(String email, int totalScore) {
+        super();
+        this.email = email;
+        this.totalScore = totalScore;
+    }
+
+
+    public String getEmail() {
+        return email;
+    }
+
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+
+    public int getTotalScore() {
+        return totalScore;
+    }
+
+
+    public void setTotalScore(int totalScore) {
+        this.totalScore = totalScore;
+    }
+
+
+
+}
